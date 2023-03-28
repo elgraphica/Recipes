71effd330e5cc29c624371afbe2b3d4bf02b2817
elgraphica <117668709+elgraphica@users.noreply.github.com>
2023-03-28 19:44:29 +0100

    made two sections so far added some style and animation with css

---
+++ script.js
@@ -0,0 +1,22 @@
+const images = document.querySelectorAll('.section-1-imgs img')
+
+let i = 1
+
+setInterval(() => {
+    i++
+
+    const image = document.querySelector('.section-1-imgs .change')
+
+    image.classList.remove('change')
+
+    if(i > images.length) {
+        images[0].classList.add('change') 
+        i = 1
+    }else {
+        image.nextElementSibling.classList.add('change')
+    }
+
+
+
+
+}, 2000)
